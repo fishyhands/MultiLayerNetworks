40b34169ac41999cca66deb4c0c79f8cb26c2460
fishyhands <139359365+fishyhands@users.noreply.github.com>
2023-08-24 23:13:07 +0800

    24/8/2023

--- networkstudies.py
+++ networkstudies.py
@@ -1,13 +1,27 @@
+# Adjecency matrix practice
+
 import matplotlib.pyplot as plt
 import networkx as nx 
+import random as rand
 
-def add_Nodes(matrix):
+graph = nx.Graph()
+# Undirected graph
+
+def links_finder(matrix):
     position = []
     for i in range(len(matrix)):
         for j in range(len(matrix[i])):
             if matrix[i][j] == 1:
-                position.append([i,j])
+                position.append([i+1,j+1])
     return position
+
+def noofNodes(position):
+    noofNodes = []
+    for i in range(len(position)):
+        for j in range(len(position[i])):
+            if position[i][j] not in noofNodes:
+                noofNodes.append(position[i][j])
+    return noofNodes
 
 
 matrix = ([0,1,1,0],
@@ -16,4 +30,10 @@ matrix = ([0,1,1,0],
           [0,1,0,0]
 )
 
-print(add_Nodes(matrix))
+
+links = links_finder(matrix)
+nodes = noofNodes(links)
+graph.add_nodes_from(nodes)
+graph.add_edges_from(links)
+nx.draw_networkx(graph,with_labels=True)
+plt.show()
