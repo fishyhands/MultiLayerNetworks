63cd6aa590650015104d91441aa2f067a7c7d8f3
fishyhands <139359365+fishyhands@users.noreply.github.com>
2023-12-27 20:28:57 +0800

    change v1,lamb1

--- networkstudies.py
+++ networkstudies.py
@@ -12,7 +12,6 @@ import ndlib.models.compartments.ConditionalComposition as cif
 mpl.use('TkAgg')
 
 
-
 ## Create Graph
 nodes = 5000
 edges = 2
@@ -29,11 +28,11 @@ plt.show()
 ## Model creation
 model = gc.CompositeModel(G)
 
-model.add_status("Susceptible")
-model.add_status("Idea 1")
-model.add_status("Idea 2")
-model.add_status("Exposed both Ideas")
-model.add_status("Susceptible again")
+model.add_status("S")
+model.add_status("1")
+model.add_status("2")
+model.add_status("C")
+
 
 
 
@@ -44,10 +43,18 @@ model.add_status("Susceptible again")
 
 # if in presence of 1 idea only, P(idea 1) = v1 or P(idea 2)= v2
 # if in presence of both, P(idea 1) = alpha1 and P(idea 2)= beta2, where alpha and beta are factors of v1 and v2 respectively
-v1 = 0.2
-v2 = 0.1
-alpha = rand.random()
-beta = rand.uniform(alpha,0) # alpha is more dominant
+v1 = 0.6
+v2 = 0.5
+
+alpha = 0.15
+beta = 0.35
+
+lamb1 = 0.3
+lamb2  = 0.25
+
+del1 = v1/lamb1
+del2 = v2/lamb2
+
 exclusive = False
 
 if exclusive == False: # exclusive influence is when alpha or beta = 0
@@ -57,68 +64,14 @@ else:
     alpha1 = alpha * v1
     beta2 = 0
 
-#cured and susceptible again
-delta1 = 0.3 
-delta2 = 0.2
-
-c_idea1 = cpm.NodeStochastic(v1, triggering_status= "Idea 1")
-c_idea2 = cpm.NodeStochastic(v2,triggering_status = "Idea 2")
-c_both1 = cpm.NodeStochastic(alpha1, triggering_status= "Idea 1")
-c_both2 = cpm.NodeStochastic(beta2, triggering_status="Idea 2")
-c_cure1 = cpm.NodeStochastic(delta1, triggering_status= "Susceptible again")
-c_cure2 = cpm.NodeStochastic(delta2, triggering_status = "Susceptible again")
-
-# Add rules
-
-model.add_rule("Susceptible", "Idea 1", c_idea1)
-model.add_rule("Susceptible", "Idea 2", c_idea2)
-
-
-"""
-Basic SIS Model
-
-model = ep.SISModel(G)
-cfg = mc.Configuration()
-cfg.add_model_parameter('beta', 0.01)
-cfg.add_model_parameter('lambda', 0.005)
-cfg.add_model_parameter("fraction_infected", 0.05)
-model.set_initial_status(cfg)
-
-
-# Degree distribution and stuff
-degree_sequence = sorted((d for n, d in G.degree()), reverse=True)
-dmax = max(degree_sequence)
-
-# Figure size and Graphs
-fig = plt.figure("Degree of a random graph", figsize=(8, 8))
-axgrid = fig.add_gridspec(5, 4)
-ax0 = fig.add_subplot(axgrid[0:3, :])
-Gcc = G.subgraph(sorted(nx.connected_components(G), key=len, reverse=True)[0])
-pos = nx.spring_layout(Gcc, seed=10396953)
-nx.draw_networkx_nodes(Gcc, pos, ax=ax0, node_size=20)
-nx.draw_networkx_edges(Gcc, pos, ax=ax0, alpha=0.4)
-ax0.set_title("Scale-Free Network G")
-ax0.set_axis_off()
-ax1 = fig.add_subplot(axgrid[3:, :2])
-ax1.plot(degree_sequence, "b-", marker="o")
-ax1.set_title("Degree Distribution Plot (2000 nodes)")
-ax1.set_ylabel("Degree")
-ax1.set_xlabel("Node Count")
-ax2 = fig.add_subplot(axgrid[3:, 2:])
-ax2.bar(*np.unique(degree_sequence, return_counts=True))
-ax2.set_title("Degree histogram")
-ax2.set_xlabel("Degree")
-ax2.set_ylabel("Node Count")
-fig.tight_layout()
-plt.show()
+c_idea1 = cpm.NodeStochastic(v1, triggering_status= "1")
+c_idea2 = cpm.NodeStochastic(v2,triggering_status = "2")
+c_both = cpm.NodeStochastic()
 
 
 
-# Visualizing Results
 
-iterations = model.iteration_bunch(200)
-trends = model.build_trends(iterations)
-viz = DiffusionTrend(model, trends)
-viz.plot()
+model.add_rule("S", "1", c_idea1)
+model.add_rule("S", "2", c_idea2)
 
-"""
+
